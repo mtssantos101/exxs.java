8725226dffb970a62ad3be9dafce4afa91ee4818
mtssantos101 <mtssantos101@gmail.com>
2023-08-28 19:10:30 -0300

    scanner básico

--- projetos/Devdojo15.java
+++ projetos/Devdojo15.java
@@ -11,6 +11,10 @@ public class Devdojo15 {
         dias [1][1]= 28;
         dias [1][2]= 19;
 
+        dias [2][0]=9;
+        dias [2][1]=11;
+        dias [2][2]=18;
+
         for (int i = 0; i < dias.length; i++) {
             for (int j = 0; j < dias.length; j++) {
                 System.out.println(dias [i][j]);
---
+++ projetos/scaner.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+import java.util.Locale;
+public class scaner{
+    public static void main(String[] args) {
+        Scanner scaner= new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite sua altura");
+        double altura = scaner.nextDouble();
+
+        System.out.println("digite seu nome completo");
+        String nome = scaner.next();
+
+        System.out.println("digite seu email");
+        String email = scaner.next();
+        
+
+        System.out.println("digite sua idade");
+        int idade = scaner.nextInt();
+        
+
+    System.out.println("oii, sou o "+nome);
+    System.out.println("tenho "+altura+"de altura");
+    System.out.println("meu emai é "+email);
+    System.out.println("tenho "+idade+" anos");
+    }
+}
